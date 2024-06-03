3695e2f66e4041a8fc820378b03920e25f561186
Casssifa(loacl) <3300595140@qq.com>
2024-06-03 22:09:34 +0800

    实现登录注册页面效果

--- KOB_Android/app/src/main/java/com/example/kob_android/activity/ModifyMyBotActivity.java
+++ KOB_Android/app/src/main/java/com/example/kob_android/activity/ModifyMyBotActivity.java
@@ -1,8 +1,6 @@
 package com.example.kob_android.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
